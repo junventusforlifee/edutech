dc0c4f5bb7ead0d41c3a06c717490fa5012e89a9
juventusforlifee <user.juventusforlifee@gmail.com>
2025-11-15 15:45:19 +0100

    update backend: multiple improvements

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -25,5 +25,8 @@ authRouter.post("/me", userAuth, me);
 authRouter.post("/send-reset-link", sendResetLink);
 authRouter.post("/reset-password", resetPassword);
 authRouter.post("/google", googleAuth);
+authRouter.get("/google", (req, res) => {
+  res.status(200).send("Google OAuth GET endpoint is working.");
+});
 
 export default authRouter;
